7d11b52bd23e166276a3102a5d05710b140e39d5
holyhloy <tlegchilin@yandex.ru>
2025-04-21 15:46:03 +0300

    project created, db settings configured

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+class BaseModel(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
+Session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
+        await conn.run_sync(BaseModel.metadata.create_all)
+
+async def get_session():
+    async with Session() as session:
+        yield session
