11c962311c6cd76f838e9be9d78b96c27f196215
holyhloy <tlegchilin@yandex.ru>
2025-04-29 16:04:06 +0300

    style: flake8 used

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,11 +14,13 @@ def app() -> FastAPI:
     app.include_router(main_router)
     return app
 
+
 @pytest_asyncio.fixture
 async def client(app: FastAPI):
     async with AsyncClient(transport=ASGITransport(app=app),
                            base_url='http://test') as client:
         yield client
+
 
 @pytest.fixture(autouse=True, scope="session")
 def init_cache():
